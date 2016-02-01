0436c77ab9124552a7f1b53021d22065c9dbeb23
Bowserinator <bowserinator@gmail.com>
2016-02-01 08:19:27 -0500

    Added basic js

---
+++ basic.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='irc.html'>IRC</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
