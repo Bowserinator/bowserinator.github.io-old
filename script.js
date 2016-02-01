f3ca64483b4e627bc1c4637450d4333ecc84af7a
Bowserinator <bowserinator@gmail.com>
2016-02-01 08:16:35 -0500

    Added javscript

---
+++ script.js
@@ -0,0 +1,14 @@
+var date1 = new Date("1/31/2016");
+var date2 = new Date();
+var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+console.log(diffDays)
+document.getElementById("foot01").innerHTML =
+"<p>Currently  " + diffDays + " days have passed since the website first came up!.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='irc.html'>IRC</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
