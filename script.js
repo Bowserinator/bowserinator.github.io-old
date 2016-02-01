190eb8694c0b0cf6fc430402d91cc96e66ff8925
Bradley Bellairs <BWBellairs@outlook.com>
2016-02-01 15:40:39 +0000

    Update script.js
    Remember only use target="_blank" for external urls outside your website e.g. http://google.com

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById("foot01").innerHTML =
 
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='index.html'>Home</a></li>" +
-"<li><a href='irc.html'>IRC</a></li>" +
-"<li><a href='about.html'>About</a></li>" +
+"<li><a href='index'>Home</a></li>" +
+"<li><a href='irc'>IRC</a></li>" +
+"<li><a href='about'>About</a></li>" +
 "</ul>";
