59f61078307dbff05abb733d708101bb64837d6e
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-02-03 18:59:24 -0500

    Use jQuery instead of raw JS

--- basic.js
+++ basic.js
@@ -1,8 +1,8 @@
 
 
-document.getElementsByClassName("nav01")[0].innerHTML =
+$(".nav01").html(
 "<ul class=\"menu\">" +
 "<li><a href=\"index.html\">Home</a></li>" +
 "<li><a href=\"irc.html\">IRC</a></li>" +
 "<li><a href=\"about.html\">About</a></li>" +
-"</ul>";
+"</ul>");
--- script.js
+++ script.js
@@ -3,12 +3,10 @@ var date2 = new Date();
 var timeDiff = Math.abs(date2.getTime() - date1.getTime());
 var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
 console.log(diffDays)
-document.getElementsByClassName("foot01")[0].innerHTML =
-"<p>Currently  " + diffDays + " days have passed since the website first came up!.</p>";
-
-document.getElementsByClassName("nav01")[0].innerHTML =
+$(".foot01").html("<p>Currently  " + diffDays + " days have passed since the website first came up!.</p>");
+$(".nav01").html(
 "<ul class=\"menu\">" +
 "<li><a href=\"index.html\">Home</a></li>" +
 "<li><a href=\"irc.html\">IRC</a></li>" +
 "<li><a href=\"about.html\">About</a></li>" +
-"</ul>";
+"</ul>");
