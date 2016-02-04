c174d3669a7898d5ac21a776ed08e89780b4a3f8
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-02-03 21:10:43 -0500

    Add the .active class to the proper link if the URL matches

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ var timeDiff = Math.abs(date2.getTime() - date1.getTime());
 var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 console.log(diffDays);
 $(".foot01 span").text(diffDays);
+// Add the .active class to the proper link if the URL matches
+$(".menu-link").each(function() {
+    if ($(this).attr("href") == location.pathname || $(this).attr("href").indexOf(location.pathname) && location.pathname != "/") {
+        $(this).addClass("active");
+    } else if (location.pathname == "/") {
+        $(".menu-link").eq(0).addClass("active");
+    }
+});
